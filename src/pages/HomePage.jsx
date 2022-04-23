5ba40097e88cf1a99ca17e82fec9e691380f4762
Kristie <kmlbrandis@gmail.com>
2022-04-23 12:51:33 +0800

    linked react to api

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { allProjects } from "../data";
 import ProjectCard from "../components/ProjectCard/ProjectCard";
 
 function HomePage() {
+    const [projectList, setProjectList] = useState();
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}projects`)
+        .then((results) => {return results.json();})
+        .then((data) => {setProjectList(data);});
+    }, []);
+
+    if (!projectList) {
+        return <h3>Loading..</h3>;
+    }
+
     return (
+
         <div>
             <h1> All Projects </h1>
         <div id="project-list">
-            {allProjects.map((projectData, key) => {
+            {projectList.map((projectData, key) => {
             return <ProjectCard key={key} projectData={projectData} />;
             })}
         </div>
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,21 +1,40 @@
-import React from "react";
-import { oneProject } from "../data";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+// import { oneProject } from "../data";
 
 function ProjectPage() {
+    const [projectData, setProjectData] = useState();
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}projects/${id}`)
+        .then((results) => {
+        return results.json();
+        })
+        .then((data) => {
+            setProjectData(data);
+        });
+    }, [id]);
+
+    if (!projectData) {
+        return <h3>Loading..</h3>;
+    }
+        
+    
     return (
          // <React.Fragment> like a div but not</React.Fragment> or <>
         <div>
-            <h2>{oneProject.title}</h2>
-            <h3>Created at: {oneProject.date_created}</h3>
-            <h3>{`Status: ${oneProject.is_open}`}</h3>
-            <h3>Project Status: {oneProject.status}</h3>
-            <h3>Project Goal: {oneProject.goal}</h3>
-            <h3>Progress %: {oneProject.progress_perc}</h3>
-            <img src={oneProject.primary_image} alt="project image"/>
-            <h3>Project Info: {oneProject.description}</h3>
+            <h2>{projectData.title}</h2>
+            <h3>Created at: {projectData.date_created}</h3>
+            <h3>{`Status: ${projectData.is_open}`}</h3>
+            <h3>Project Status: {projectData.status}</h3>
+            <h3>Project Goal: {projectData.goal}</h3>
+            <h3>Progress %: {projectData.progress_perc}</h3>
+            <img src={projectData.primary_image} alt="project image"/>
+            <h3>Project Info: {projectData.description}</h3>
             <h3>Pledges:</h3>
             <ul>
-                {oneProject.pledges.map((pledgeData, key) => {
+                {projectData.pledges.map((pledgeData, key) => {
                     return (
                         <li>
                             {pledgeData.amount} from {pledgeData.supporter}
